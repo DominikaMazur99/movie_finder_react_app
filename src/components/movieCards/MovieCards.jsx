494cd7297b383723ec377249fe869ae71fb64ab0
Dominika Mazur <d.mazur@aotm.gov.pl>
2023-05-29 10:51:23 +0200

    favorites

--- src/components/movieCards/MovieCards.jsx
+++ src/components/movieCards/MovieCards.jsx
@@ -10,140 +10,132 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useContext } from "react";
 import { AddToFavouriteContext } from "../../context/AddToFavouritesContext";
 import {
-    getDataFromLocalStorage,
-    saveDataInLocalStorage,
+  getDataFromLocalStorage,
+  saveDataInLocalStorage,
 } from "../../helpers/localStorageFunctions";
 
 function MovieCards({ category }) {
-    const { isFavourite, setIsFavourite } = useContext(AddToFavouriteContext);
-    const [movies, setMovies] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState("");
+  const { isFavourite, setIsFavourite } = useContext(AddToFavouriteContext);
+  const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        let endpoint;
-        if (category === "popular") {
-            endpoint = `sort_by=popularity.desc&page=${currentPage}`;
-        } else if (category === "kids") {
-            endpoint = `certification_country=US&certification.lte=G&sort_by=popularity.desc&page=${currentPage}`;
-        } else if (category === "science_fiction") {
-            endpoint = `with_genres=878&sort_by=vote_average.desc&page=${currentPage}`;
-        }
+  useEffect(() => {
+    let endpoint;
+    if (category === "popular") {
+      endpoint = `sort_by=popularity.desc&page=${currentPage}`;
+    } else if (category === "kids") {
+      endpoint = `certification_country=US&certification.lte=G&sort_by=popularity.desc&page=${currentPage}`;
+    } else if (category === "science_fiction") {
+      endpoint = `with_genres=878&sort_by=vote_average.desc&page=${currentPage}`;
+    }
 
-        setLoading(true);
-        fetchMovies("GET", null, {}, endpoint)
-            .then((data) => {
-                setMovies(data.results);
-                setTotalPages(data.total_pages);
-            })
-            .catch((err) => console.log(err))
-            .finally(() => setLoading(false));
-    }, [currentPage, category]);
+    setLoading(true);
+    fetchMovies("GET", null, {}, endpoint)
+      .then((data) => {
+        setMovies(data.results);
+        setTotalPages(data.total_pages);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [currentPage, category]);
 
-    useEffect(() => {
-        saveDataInLocalStorage("favourite_movies", isFavourite);
-    }, [isFavourite]);
+  useEffect(() => {
+    saveDataInLocalStorage("favourite_movies", isFavourite);
+    const favouritesMovies = getDataFromLocalStorage("favourite_movies");
+    setIsFavourite(favouritesMovies || []);
+  }, [isFavourite, setIsFavourite]);
 
-    useEffect(() => {
-        const favouritesMovies = getDataFromLocalStorage("favourite_movies");
-        setIsFavourite(favouritesMovies || []);
-    }, []);
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      searchMovies("GET", null, {}, `query=${searchTerm}`)
+        .then((data) => {
+          setMovies(data.results);
+          setTotalPages(data.total_pages);
+        })
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }
+  }, [searchTerm]);
 
-    useEffect(() => {
-        if (searchTerm) {
-            setLoading(true);
-            searchMovies("GET", null, {}, `query=${searchTerm}`)
-                .then((data) => {
-                    setMovies(data.results);
-                    setTotalPages(data.total_pages);
-                })
-                .catch((err) => console.log(err))
-                .finally(() => setLoading(false));
-        }
-    }, [searchTerm]);
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    const handlePageClick = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  const addToFavourite = (movie) => {
+    if (isFavourite.some((favMovie) => favMovie.title === movie.title)) {
+      setIsFavourite((prevFavourites) =>
+        prevFavourites.filter((item) => item.title !== movie.title)
+      );
+    } else {
+      setIsFavourite((prevFavourites) => [...prevFavourites, movie]);
+    }
+  };
 
-    const addToFavourite = (movie) => {
-        if (isFavourite.some((favMovie) => favMovie.title === movie.title)) {
-            setIsFavourite((prevFavourites) =>
-                prevFavourites.filter((item) => item.title !== movie.title)
-            );
-        } else {
-            setIsFavourite((prevFavourites) => [...prevFavourites, movie]);
-        }
-    };
-
-    const tmdbImageUrl = "https://image.tmdb.org/t/p";
-    const imageSize = "w500";
-    console.log(isFavourite);
-    return (
+  const tmdbImageUrl = "https://image.tmdb.org/t/p";
+  const imageSize = "w500";
+  console.log(isFavourite);
+  return (
+    <>
+      <MainSearcher searchTerm={searchTerm} handleSearch={handleSearch} />
+      {loading ? (
+        <div className="text-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
         <>
-            <MainSearcher searchTerm={searchTerm} handleSearch={handleSearch} />
-            {loading ? (
-                <div className="text-center">
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                </div>
-            ) : (
-                <>
-                    <div className="movie-card-container">
-                        {movies?.map((movie) => (
-                            <Card
-                                key={`movie-${movie.title}`}
-                                className="movie-card"
-                            >
-                                <Card.Img
-                                    variant="top"
-                                    src={`${tmdbImageUrl}/${imageSize}${movie.poster_path}`}
-                                />
-                                <Card.Body>
-                                    <Card.Title style={{ fontSize: "1rem" }}>
-                                        {movie.title}
-                                    </Card.Title>
-                                    <Card.Text style={{ fontSize: "0.75rem" }}>
-                                        {`${movie.overview?.slice(0, 75)}...`}
-                                    </Card.Text>
-                                    <div className="favourite-add">
-                                        <div
-                                            className="heart-icon"
-                                            onClick={() => {
-                                                addToFavourite(movie);
-                                            }}
-                                        >
-                                            {isFavourite.some(
-                                                (favMovie) =>
-                                                    favMovie.title ===
-                                                    movie.title
-                                            ) ? (
-                                                <FaHeart />
-                                            ) : (
-                                                <FaRegHeart />
-                                            )}
-                                        </div>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        ))}
+          <div className="movie-card-container">
+            {movies?.map((movie) => (
+              <Card key={`movie-${movie.title}`} className="movie-card">
+                <Card.Img
+                  variant="top"
+                  src={`${tmdbImageUrl}/${imageSize}${movie.poster_path}`}
+                />
+                <Card.Body>
+                  <Card.Title style={{ fontSize: "1rem" }}>
+                    {movie.title}
+                  </Card.Title>
+                  <Card.Text style={{ fontSize: "0.75rem" }}>
+                    {`${movie.overview?.slice(0, 75)}...`}
+                  </Card.Text>
+                  <div className="favourite-add">
+                    <div
+                      className="heart-icon"
+                      onClick={() => {
+                        addToFavourite(movie);
+                      }}
+                    >
+                      {isFavourite.some(
+                        (favMovie) => favMovie.title === movie.title
+                      ) ? (
+                        <FaHeart />
+                      ) : (
+                        <FaRegHeart />
+                      )}
                     </div>
-                    <Pagination
-                        currentPage={currentPage}
-                        handlePageClick={handlePageClick}
-                        totalPages={totalPages}
-                    />
-                </>
-            )}
+                  </div>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+          <Pagination
+            currentPage={currentPage}
+            handlePageClick={handlePageClick}
+            totalPages={totalPages}
+          />
         </>
-    );
+      )}
+    </>
+  );
 }
 
 export default MovieCards;
--- src/components/navbar/MainNavbar.jsx
+++ src/components/navbar/MainNavbar.jsx
@@ -1,34 +1,76 @@
-import { Container } from "react-bootstrap";
+import { Container, Dropdown } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import { BiMoviePlay } from "react-icons/bi";
+import { FaHeart } from "react-icons/fa";
+import React, { useContext, useState } from "react";
+import { AddToFavouriteContext } from "../../context/AddToFavouritesContext";
+
 import "./MainNavbar.scss";
 
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <div
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+  </div>
+));
+
 function MainNavbar() {
-    return (
-        <>
-            <Navbar className="flex-column navbar-section ">
-                <Container>
-                    <Navbar.Brand href="/" className="text-left ">
-                        <div className="d-flex align-items-center">
-                            <div>
-                                <BiMoviePlay size={30} />
-                            </div>
-                            <div className="ml-2">MovieFinder</div>
-                        </div>
-                    </Navbar.Brand>
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/">Popular</Nav.Link>
-                        <Nav.Link href="/kids">Kids</Nav.Link>
-                        <Nav.Link href="/science_fiction">
-                            Science Fiction
-                        </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    );
+  const { isFavourite } = useContext(AddToFavouriteContext);
+
+  const [showFavouriteMovies, setShowFavouriteMovies] = useState(false);
+
+  const handleShowFavourites = () => {
+    setShowFavouriteMovies(!showFavouriteMovies);
+  };
+
+  const handleCloseList = () => {
+    setShowFavouriteMovies(false);
+  };
+  return (
+    <>
+      <Navbar className="flex-column navbar-section ">
+        <Container>
+          <Navbar.Brand href="/" className="text-left ">
+            <div className="d-flex align-items-center">
+              <div>
+                <BiMoviePlay size={30} />
+              </div>
+              <div className="ml-2">MovieFinder</div>
+            </div>
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <FaHeart onClick={handleShowFavourites} />
+
+              {showFavouriteMovies && (
+                <Dropdown show={showFavouriteMovies} onToggle={handleCloseList}>
+                  <Dropdown.Toggle
+                    as={CustomToggle}
+                    id="dropdown-heart-toggle"
+                  ></Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {isFavourite.map(({ id, original_title }) => (
+                      <Dropdown.Item key={id}>{original_title}</Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
+            </Nav.Link>
+            <Nav.Link href="/">Popular</Nav.Link>
+            <Nav.Link href="/kids">Kids</Nav.Link>
+            <Nav.Link href="/science_fiction">Science Fiction</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default MainNavbar;
